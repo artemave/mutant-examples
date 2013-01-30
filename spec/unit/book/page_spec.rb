f48bfa8f98653c42186f71c881a2baa48c419b3f
Markus Schirp <mbj@seonic.net>
2013-01-30 14:30:28 +0100

    Update spec/unit/book/page_spec.rb

--- spec/unit/book/page_spec.rb
+++ spec/unit/book/page_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 
 describe Book, '#page' do
-  subject(:book) { described_class.new }
+  subject(:book) { described_class.new(pages) }
 
   let(:page) { Page.new(1) }
 
-  before { book.add_page(page) }
-
   context 'when page exists' do
+    let(:pages) { [page] }
+    
     it 'should return page' do
       expect(book.page(1)).to be(page)
     end
   end
 
   context 'when page does not exist' do
+    let(:pages) { [] }
+  
     it 'should raise error' do
       expect { book.page(2) }.to raise_error(
         RuntimeError, "Book does not have a page with number: 2"
